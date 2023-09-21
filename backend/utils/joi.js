48718d722192823e33d2af429043b047092999c5
nasith <nasith@pepul.com>
2023-09-21 15:13:45 +0530

    Dynamic Forms

--- backend/utils/joi.js
+++ backend/utils/joi.js
@@ -1,23 +1,22 @@
 const Joi = require('joi');
 
 const schema = Joi.object({
-    productID:Joi.number().min(100000).max(999999),
-    rate:Joi.number(),
-    discount:Joi.number(),
-    price:Joi.number()
+    productID: Joi.number().min(100000).max(999999),
+    rate: Joi.number(),
+    discount: Joi.number(),
+    price: Joi.number()
 });
 
-const validate = (req,res,next) => {
-    const productID =  Math.floor(100000 + Math.random() * 900000);
+const validate = (req, res, next) => {
+    const productID = Math.floor(100000 + Math.random() * 900000);
     const rate = req.body.rate;
     const discount = req.body.discount;
     const price = req.body.price;
-    const { error } = schema.validate({productID,rate,discount,price});
-    if(error){
-        return res.status(401).json({statuscode:401,error:error.message})
+    const { error } = schema.validate({ productID, rate, discount, price });
+    if (error) {
+        return res.status(401).json({ statuscode: 401, error: error.message })
     }
     next();
 }
 
-module.exports ={validate};
-    
+module.exports = { validate };
